56ab73151b802e1324f40cd7f739ba99d8e27b46
Ahmedzaky280 <75086464+Ahmedzaky280@users.noreply.github.com>
2021-04-22 01:50:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'shared/nav.php' ?>
+
+<br>
+<h3 class="text-center" style = "font-family:courier,arial,helvetica;"> Welcome to ZAKYR for Hr services </h1>
+
+
+<img src="The best HR reports to produce for your business.png" style=" display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%; margin-bottom:auto; ">
+<div class="card"  style="margin-top: 0px;  position: fixed;
+    bottom: 0px; width:25%" >
+  <h6 class="card-header">About</h6>
+  <div class="card-body">
+    <h5 class="card-title">For more information</h5>
+    
+    <footer >
+<div></div>
+  <p>Author: Ahmed zaky</p>
+
+</footer>
+    <a href="https://minecraftteam140@gmail.com" class="btn btn-primary"> Email: minecraftteam140@gmail.com</a>
+    <p style="margin-top: 0px;" class="text-center">@Copyrights Allrights Reserved</p>
+  </div>
+</div>
